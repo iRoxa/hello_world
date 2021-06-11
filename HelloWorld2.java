27e63ba95f5ef2337f5daf6bac5a5169b4e003b2
Rossa <roi@flarepolaris.com>
2021-06-11 12:15:04 +0200

    aggiunta commento

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -2,6 +2,7 @@
  {
 	 public static void main(String args[])
 	 {
+		 // il mio commento :)
 		 System.out.println("************");
 		 System.out.println("Hello World!");
 		 System.out.println("************");
