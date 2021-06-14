b18539cc15c77a26759ae3dc93ba54edeca47931
iRoxa <85740287+iRoxa@users.noreply.github.com>
2021-06-14 18:25:56 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@
 	 public static void main(String args[])
 	 {
 		 System.out.println("************");
-		 System.out.println("Hello World Rossana!");
+		 System.out.println("Hello World iRoxa!");
 		 System.out.println("************");
 	 }
  }
